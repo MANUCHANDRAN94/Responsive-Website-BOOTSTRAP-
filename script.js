cdbb0098d09893fb88e188e88a5c41bf26497508
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-09-16 14:21:54 +0530

    works good till now

--- script.js
+++ script.js
@@ -6,3 +6,60 @@
 //     });
 
 // });
+
+// carousel for portfolio
+$('.owl-portfolio').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    dotsEach:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        550:{
+            items:2
+        },
+        900:{
+            items:3
+        }
+    }
+})
+
+// carousel ofr our team
+$('.owl-team').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    dotsEach:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        500:{
+            items:3
+        },
+        900:{
+            items:4
+        }
+    }
+})
+
+//carousel for blog
+$('.owl-blog').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    dotsEach:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        900:{
+            items:3
+        }
+    }
+})
